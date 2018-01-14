ea99ec69e63a34002e59ff8707a802e190d4e1a4
risca <risca@reti>
2018-01-14 12:26:18 +0100

    fixed temporary timeout error

--- saltwebgui/decorators.py
+++ saltwebgui/decorators.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 """Placeholder for custom decorators"""
-
+import sys
 from functools import wraps
 from flask import abort, flash, redirect, url_for
 from flask_login import current_user, logout_user
 from pepper import PepperException
+from urllib2 import URLError
 
 
 def admin_required(f):
@@ -29,4 +30,12 @@ def api_status(f):
                 flash(error, 'warning')
             logout_user()
             return redirect(url_for('frontend.login'))
+        except URLError as error:
+            exc_info = sys.exc_info()
+            if error.errno == 111:
+                flash(error, 'warning')
+                return False
+            else:
+                raise exc_info[0], exc_info[1], exc_info[2]
+
     return decorated_function
